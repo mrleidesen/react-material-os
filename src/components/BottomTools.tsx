41b77294472d12d60e4751a09ea130d92676a2d4
mrleidesen <mrleidesen@outlook.com>
2021-05-07 11:09:34 +0800

    feat: add active icon

--- src/components/BottomTools.tsx
+++ src/components/BottomTools.tsx
@@ -1,41 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function BottomTools() {
+  const icons = [
+    {
+      id: 1,
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
+        </svg>
+      )
+    },
+    {
+      id: 2,
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clipRule="evenodd" />
+        </svg>
+      )
+    },
+    {
+      id: 3,
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-gray-600" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.293 1.293a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 01-1.414-1.414L7.586 10 5.293 7.707a1 1 0 010-1.414zM11 12a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
+        </svg>
+      )
+    },
+    {
+      id: 4,
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd" />
+        </svg>
+      )
+    },
+  ]
+  const [activeId, setActiveId] = useState<number | null>(null)
+
+  const onActive = (id: number) => {
+    setActiveId(id)
+  }
+
   return (
     <div className="h-20 w-full box-border px-8 py-2 flex items-center justify-center">
-      <div className="bg-white bg-opacity-90 rounded-3xl min-w-min h-16 box-border p-4 shadow flex items-center justify-center">
-        <RoundIcon>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-            <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
-          </svg>
-        </RoundIcon>
-        <RoundIcon>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-red-500" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
-          </svg>
-        </RoundIcon>
-        <RoundIcon>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M3 5a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2h-2.22l.123.489.804.804A1 1 0 0113 18H7a1 1 0 01-.707-1.707l.804-.804L7.22 15H5a2 2 0 01-2-2V5zm5.771 7H5V5h10v7H8.771z" clipRule="evenodd" />
-          </svg>
-        </RoundIcon>
-        <RoundIcon>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-green-500" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd" />
-          </svg>
-        </RoundIcon>
+      <div className="bg-white bg-opacity-60 rounded-3xl min-w-min h-16 box-border p-4 shadow flex items-center justify-center">
+        {
+          icons.map((icon) => (
+            <RoundIcon key={icon.id} onClick={() => onActive(icon.id)} className={activeId === icon.id ? 'round-icon--active' : ''}>
+              {icon.icon}
+            </RoundIcon>
+          ))
+        }
       </div>
     </div>
   )
 }
 
 interface RoundIconProps {
-  children?: React.ReactNode
+  children?: React.ReactNode;
+  onClick?: React.MouseEventHandler;
+  className?: string;
+  key?: string | number;
 }
-const RoundIcon = (props: RoundIconProps) => {
+const RoundIcon = ({ children, className, ...rest }: RoundIconProps) => {
   return (
-    <div className="w-14 h-14 rounded-xl bg-black bg-opacity-5 mx-1 relative transform hover:shadow-lg hover:mx-5 hover:z-10 hover:bg-opacity-100 hover:bg-white hover:-translate-y-4 transition-all hover:scale-150 flex justify-center items-center duration-200">
-      {props.children}
+    <div className={`round-icon ${className}`} {...rest}>
+      {children}
     </div>
   )
 }
