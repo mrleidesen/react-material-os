972565c58ab2ef77857686542dc5d3e82e9a6f53
mrleidesen <mrleidesen@outlook.com>
2021-05-08 16:35:30 +0800

    fix: using heroicons

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -1,46 +1,46 @@
 import React from 'react'
+import {
+  DesktopComputerIcon,
+  MusicNoteIcon,
+  VideoCameraIcon,
+  DownloadIcon,
+  CogIcon
+} from '@heroicons/react/outline'
+import {
+  DocumentTextIcon,
+  FolderOpenIcon
+} from '@heroicons/react/solid'
 
 export default function File() {
   const menu = [
     {
       name: 'Desktop',
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-400" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M3 5a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2h-2.22l.123.489.804.804A1 1 0 0113 18H7a1 1 0 01-.707-1.707l.804-.804L7.22 15H5a2 2 0 01-2-2V5zm5.771 7H5V5h10v7H8.771z" clipRule="evenodd" />
-        </svg>
+        <DesktopComputerIcon className="h-full w-full text-blue-400" />
       )
     },
     {
       name: 'Music',
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" />
-        </svg>
+        <MusicNoteIcon className="h-full w-full text-red-500" />
       )
     },
     {
       name: 'Video',
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
-        </svg>
+        <VideoCameraIcon className="h-full w-full text-green-500" />
       )
     },
     {
       name: 'Download',
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z" clipRule="evenodd" />
-        </svg>
+        <DownloadIcon className="h-full w-full" />
       )
     },
     {
       name: 'Settings',
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
+        <CogIcon className="h-full w-full text-yellow-700" />
       )
     }
   ]
@@ -56,14 +56,10 @@ export default function File() {
   ]
   const fileHash: {[key: string]: React.ReactNode} = {
     'file': (
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full text-gray-300" viewBox="0 0 20 20" fill="currentColor">
-        <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd" />
-      </svg>
+      <DocumentTextIcon className="h-full w-full text-gray-300" />
     ),
     'folder': (
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-        <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
-      </svg>
+      <FolderOpenIcon className="h-full w-full text-blue-500" />
     )
   }
   return (
@@ -75,7 +71,7 @@ export default function File() {
               key={item.name}
               className="w-full h-10 flex items-center box-border px-3 border-b text-sm text-gray-600 select-none cursor-pointer hover:bg-gray-100 truncate"
             >
-              {item.icon}
+              <div className="w-5 h-5">{item.icon}</div>
               <span className="ml-2">{item.name}</span>
             </div>
           ))
