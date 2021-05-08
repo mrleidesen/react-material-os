1c3091febc3104e237a50f9a22fa2c0691ede276
mrleidesen <mrleidesen@outlook.com>
2021-05-08 11:24:31 +0800

    fix: terminal bugs

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -4,34 +4,47 @@ import 'xterm/css/xterm.css'
 
 export default function TerminalApp() {
   const el = useRef<HTMLDivElement>(null)
+  const term = new Terminal()
   let cwd = ""
 
   useEffect(() => {
-    const term = new Terminal()
-    if (el.current) {
-      term.open(el.current)
-      term.writeln('Welcome to \x1B[1;3;31mReact Material OS\x1B[0m')
-      term.write('$root: ')
-      term.onKey((key) => {
-        if (key.key === '\r') {
-          term.write(`\n\rCan not found command '\x1B[1;3;31m${cwd}\x1B[0m'\n\r$root: `)
-          cwd = ""
-        } else {
-          term.write(key.key)
-          cwd += key.key
-        }
-      })
-    }
+    initTerminal()
 
     return () => {
-      if (el.current) {
-        term.dispose()
-      }
+      disposeTerminal()
     }
   }, [])
 
+  const initTerminal = () => {
+    if (el.current) {
+      term.open(el.current)
+      term.write('Welcome to \x1B[1;3;31mReact Material OS\x1B[0m\n\r$root: ')
+      term.onKey((key) => {
+        const keyName = key.domEvent.key
+        writeInTerminal(key.key, keyName)
+      })
+    }
+  }
+
+  const writeInTerminal = (char: string, key: string) => {
+    if (key === 'Enter') {
+      term.write(`\n\rCan not found command '\x1B[1;3;31m${cwd}\x1B[0m'\n\r$root: `)
+      cwd = ""
+    } else if (key === 'Backspace') {
+      term.write('\b \b')
+      cwd = cwd.slice(0, cwd.length - 1)
+    } else {
+      term.write(char)
+      cwd += char
+    }
+  }
+
+  const disposeTerminal = () => {
+    term.dispose()
+  }
+
   return (
-    <div className="w-full h-full bg-black box-border p-1" ref={el}>
+    <div className="w-full h-auto bg-black box-border p-1" ref={el}>
     </div>
   )
 }
