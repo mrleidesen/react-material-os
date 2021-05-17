34fc833c51c4dded9815ba10030ceed5ef42516e
mrleidesen <mrleidesen@outlook.com>
2021-05-17 09:24:57 +0800

    feat: file select

--- src/components/apps/File.tsx
+++ src/components/apps/File.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   DesktopComputerIcon,
   MusicNoteIcon,
@@ -12,48 +12,74 @@ import {
 } from '@heroicons/react/solid'
 
 export default function File() {
+  const [activeId, setActiveId] = useState(1)
   const menu = [
     {
+      id: 1,
       name: 'Desktop',
       icon: (
         <DesktopComputerIcon className="h-full w-full text-blue-400" />
-      )
+      ),
+      children: [
+        {
+          name: 'README.md',
+          type: 'file'
+        },
+        {
+          name: 'THIS_FOLDER',
+          type: 'folder'
+        },
+        {
+          name: 'SECOND_FOLDER',
+          type: 'folder'
+        },
+      ]
     },
     {
+      id: 2,
       name: 'Music',
       icon: (
         <MusicNoteIcon className="h-full w-full text-red-500" />
-      )
+      ),
+      children: [
+        {
+          name: 'Rick Rock',
+          type: 'folder'
+        },
+        {
+          name: 'Halo OST',
+          type: 'folder'
+        },
+        {
+          name: 'Half Life OST',
+          type: 'folder'
+        },
+        {
+          name: 'test.mp3',
+          type: 'file'
+        },
+      ]
     },
     {
+      id: 3,
       name: 'Video',
       icon: (
         <VideoCameraIcon className="h-full w-full text-green-500" />
-      )
+      ),
+      children: [
+      ]
     },
     {
+      id: 4,
       name: 'Download',
       icon: (
         <DownloadIcon className="h-full w-full" />
-      )
-    },
-    {
-      name: 'Settings',
-      icon: (
-        <CogIcon className="h-full w-full text-yellow-700" />
-      )
-    }
-  ]
-  const files = [
-    {
-      name: 'Readme.md',
-      type: 'file'
-    },
-    {
-      name: 'Hello Folder',
-      type: 'folder'
+      ),
+      children: [
+      ]
     },
   ]
+
   const fileHash: {[key: string]: React.ReactNode} = {
     'file': (
       <DocumentTextIcon className="h-full w-full text-gray-300" />
@@ -68,8 +94,9 @@ export default function File() {
         {
           menu.map(item => (
             <div 
-              key={item.name}
-              className="w-full h-10 flex items-center box-border px-3 border-b text-sm text-gray-600 select-none cursor-pointer hover:bg-gray-100 truncate"
+              key={item.id}
+              className={`w-full h-10 flex items-center box-border px-3 border-b text-sm text-gray-600 select-none cursor-pointer hover:bg-gray-100 truncate ${activeId === item.id ? 'font-semibold' : ''}`}
+              onClick={() => setActiveId(item.id)}
             >
               <div className="w-5 h-5">{item.icon}</div>
               <span className="ml-2">{item.name}</span>
@@ -77,17 +104,17 @@ export default function File() {
           ))
         }
       </div>
-      <div className="flex-1 h-auto flex box-border p-3">
+      <div className="flex flex-1 flex-wrap content-start box-border p-3 menus-item">
         {
-          files.map(file => (
+          menu.filter(v => v.id === activeId)[0].children.map(file => (
             <div 
               key={file.name}
-              className="h-24 w-24 rounded-lg flex flex-col justify-center items-center select-none hover:bg-gray-800 hover:bg-opacity-5 cursor-pointer mr-2 mb-2"
+              className="h-24 w-24 box-border px-1 overflow-hidden rounded-lg flex flex-col justify-center items-center select-none hover:bg-gray-800 hover:bg-opacity-5 cursor-pointer mr-2"
             >
               <div className="h-16 w-16">
                 {fileHash[file.type]}
               </div>
-              <span className="text-gray-600 font-semibold text-xs">{file.name}</span>
+              <span className="w-full text-center text-gray-600 font-semibold text-xs truncate">{file.name}</span>
             </div>
           ))
         }
