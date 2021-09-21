695c8eb7aa50da6b78b1ea7332eace6246df2716
mrleidesen <mrleidesen@outlook.com>
2021-09-21 09:41:40 +0800

    style: format file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {
-      zIndex: ['hover', 'visited', 'active'],
-      margin: ['hover'],
-      backgroundColor: ['hover', 'active', 'focus'],
+      zIndex: ["hover", "visited", "active"],
+      margin: ["hover"],
+      backgroundColor: ["hover", "active", "focus"],
     },
   },
   plugins: [],
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import path from 'path'
+import path from "path";
 
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   esbuild: {
-    jsxInject: "import React from 'react'"
+    jsxInject: "import React from 'react'",
   },
-  base: "/react-material-os/"
-})
+  base: "/react-material-os/",
+});
